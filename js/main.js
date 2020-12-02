4435427f095880ac7ecab53a2772f45824fe1d0d
jssmm <jessenalexmercado@gmail.com>
2020-12-01 22:04:47 -0600

    Acabada la seccion del menu-programa con animaciones en JQuery

--- js/main.js
+++ js/main.js
@@ -148,3 +148,22 @@
 
     }); // DOM CONTENT LOADED
 })();
+
+
+$(function() {
+
+    //programa de conferencias
+    $('.ocultar').hide();
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function(){
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+
+    });
+});
